85fe88beca1124653d9e8d9391233b6ffd7f9d2d
Snaiky11 <30751506+Snaiky11@users.noreply.github.com>
2017-08-06 22:43:21 +0200

    Menu admin + routes (a voir)

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -14,4 +14,21 @@ class DefaultController extends Controller
     {
         return $this->render('AdminBundle::index.html.twig');
     }
+    
+    /**
+     * @Route("/src/AdminBundle/Ressources/views/a_news.html.twig", name="a_news")
+     */
+    public function a_newsAction()
+    {
+        return $this->render('AdminBundle::a_news.html.twig');
+    }
+    
+      /**
+     * @Route("/src/AdminBundle/Ressources/views/a_users.html.twig", name="a_users")
+     */
+    public function a_usersAction()
+    {
+        return $this->render('AdminBundle::a_users.html.twig');
+    }
+    
 }
