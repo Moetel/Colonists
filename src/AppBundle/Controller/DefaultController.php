06857cfe6a3dc04c2138c55e3823f5620fa02566
MarcV <marc.vanren@gmail.com>
2017-08-13 05:25:58 +0200

    Start profile page + friend list page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,15 +15,31 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
+        $user = $this->getUser();
+
         $specified_page = $request->get('page');
 
-        if ($specified_page != "" && $this->getUser() && $this->isGranted('IS_AUTHENTICATED_FULLY'))
+        if ($specified_page != "" && $user && $this->isGranted('IS_AUTHENTICATED_FULLY'))
             return $this->redirectToRoute('homepage');
-        else if (!$specified_page && !$this->getUser() && !$this->isGranted('IS_AUTHENTICATED_FULLY'))
+        else if (!$specified_page && !$user && !$this->isGranted('IS_AUTHENTICATED_FULLY'))
             return $this->redirectToRoute('homepage', array('page' => 'login'));
 
         $news = $em->getRepository('AppBundle:News')->getLatestNewsOrderByPublishedAt();
 
         return $this->render('AppBundle::homepage.html.twig', array('news' => $news, 'specified_page' => $specified_page));
+    }
+
+    /**
+     * @Route("/friend-list", name="friend_list")
+     */
+    public function friendListAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $this->getUser();
+
+        $friend_list = $user->getFriends();
+
+        return $this->render('AppBundle::friend_list.html.twig', array('friend_list' => $friend_list));
     }
 }
